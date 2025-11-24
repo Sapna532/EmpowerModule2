bc309fe993baab22813df2ad297209694656da0f
Sapna532 <sapnaydv37@gmail.com>
2025-11-24 23:36:31 +0530

    que16 done

---
+++ ModernJS/que16.js
@@ -0,0 +1,21 @@
+
+
+
+
+// a) 
+
+javascript
+const isEven = (n) => n % 2 === 0;
+
+
+// b) 
+
+javascript
+result = (marks >= 35) ? "Pass" : "Fail";
+
+// c)
+javascript
+const greet = (name) => console.log("Hello, " + (name ? name : "Guest"));
+
+
+
