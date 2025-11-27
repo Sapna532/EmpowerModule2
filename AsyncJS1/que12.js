f5bc0eab841b6a3bcbe22d3309f9925c4d10db0e
Sapna532 <sapnaydv37@gmail.com>
2025-11-27 23:48:32 +0530

    asyncjs1 done

---
+++ AsyncJS1/que12.js
@@ -0,0 +1,10 @@
+// Print "message 1" immediately
+console.log("message 1");
+
+// Use setTimeout to print "message 2" after 2 seconds
+setTimeout(function () {
+  console.log("message 2 after 2 seconds");
+}, 2000);
+
+// Print "message 3" immediately after "message 1"
+console.log("message 3");
---
+++ AsyncJS1/que14.js
@@ -0,0 +1,15 @@
+
+
+console.log("Begin");
+
+setTimeout(() => {
+    console.log("Timeout Task");
+}, 0); 
+
+Promise.resolve().then(() => {
+    console.log("Promise Task");
+});
+
+console.log("End");
+
+
